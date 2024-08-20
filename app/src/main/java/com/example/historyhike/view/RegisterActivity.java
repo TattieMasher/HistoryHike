617c7d28b55c968977008cb669f42fc778fe2243
TattieMasher <alexandermccaughran@yahoo.com>
2024-08-20 22:49:38 +0100

    Created a registration screen matching design spec (not yet functional either).

---
+++ app/src/main/java/com/example/historyhike/view/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.historyhike.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.historyhike.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
